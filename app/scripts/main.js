f9b56346fb0bc09b6c26389964dc54b798e32a3c
xming13 <xiaoming33@gmail.com>
2015-10-10 16:00:07 +0800

    clean up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,4 @@
 function init() {
-    var circles = [];
     var CIRCLE_RADIUS = 50;
     var CIRCLE_DIAMETER = CIRCLE_RADIUS * 2;
     var SPACING = 10;
@@ -139,6 +138,7 @@ function init() {
                 .wait(3.5 * petalDuration)
                 .set({alpha: 1})
                 .to({x: -8, y: -4}, petalDuration, createjs.Ease.getBackOut(1))
+                .wait(200)
                 .call(container2Start);
         });
 
@@ -159,8 +159,6 @@ function init() {
     container1.addChild(segmentGround);
     container1.addChild(flowerWrapper);
 
-    circles.push(container1);
-
     // Card 2: Tree
     var container2 = new createjs.Container();
     container2.x = 3 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS;
@@ -175,7 +173,6 @@ function init() {
     segmentSky.graphics.beginFill(COLOR_SKY)
         .arc(0, 0, CIRCLE_RADIUS, 150 * Math.PI / 180, 30 * Math.PI / 180);
     container2.addChild(segmentSky);
-    circles.push(container2);
 
     function container2Start() {
 //        createjs.Tween.get(flowerWrapper).to({x: 2 * CIRCLE_DIAMETER + SPACING}, 500, createjs.Ease.circOut);
@@ -202,32 +199,31 @@ function init() {
         }
 
         var subContainerDuration = 500;
+        var dist = 1.5 * CIRCLE_RADIUS;
         createjs.Tween.get(subContainers[0]).wait(subContainerDuration)
-            .to({x: -1 * (CIRCLE_DIAMETER + SPACING), y: -1 * (CIRCLE_DIAMETER + SPACING)}, subContainerDuration, createjs.Ease.cubicIn);
+            .to({x: -dist, y: -dist}, subContainerDuration, createjs.Ease.cubicIn);
         createjs.Tween.get(subContainers[1]).wait(1.5 * subContainerDuration)
-            .to({x: 1 * (CIRCLE_DIAMETER + SPACING), y: -1 * (CIRCLE_DIAMETER + SPACING)}, subContainerDuration, createjs.Ease.cubicIn);
+            .to({x: dist, y: -dist}, subContainerDuration, createjs.Ease.cubicIn);
         createjs.Tween.get(subContainers[2]).wait(2 * subContainerDuration)
-            .to({x: -1 * (CIRCLE_DIAMETER + SPACING), y: 1 * (CIRCLE_DIAMETER + SPACING)}, subContainerDuration, createjs.Ease.cubicIn);
+            .to({x: -dist, y: dist}, subContainerDuration, createjs.Ease.cubicIn);
         createjs.Tween.get(subContainers[3]).wait(2.5 * subContainerDuration)
-            .to({x: 1 * (CIRCLE_DIAMETER + SPACING), y: 1 * (CIRCLE_DIAMETER + SPACING)}, subContainerDuration, createjs.Ease.cubicIn)
-            .call(container2Complete);
-
-        function container2Complete() {
-            var delay = 500;
-            var duration = 300;
-            createjs.Tween.get(subContainers[0]).wait(delay).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
-            createjs.Tween.get(subContainers[1]).wait(delay).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
-            createjs.Tween.get(subContainers[2]).wait(delay).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
-            createjs.Tween.get(subContainers[3]).wait(delay).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut)
-                .call(container3Start);
-        }
+            .to({x: dist, y: dist}, subContainerDuration, createjs.Ease.cubicIn)
+            .call(function () {
+                var delay = 500;
+                var duration = 300;
+                createjs.Tween.get(subContainers[0]).wait(delay).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
+                createjs.Tween.get(subContainers[1]).wait(delay).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
+                createjs.Tween.get(subContainers[2]).wait(delay).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
+                createjs.Tween.get(subContainers[3]).wait(delay).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut)
+                    .wait(200)
+                    .call(container3Start);
+            });
     }
 
     // Card 3: Cat
     var container3 = new createjs.Container();
     container3.x = 3 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS;
     container3.y = CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS;
-    circles.push(container3);
 
     var segmentGround3 = new createjs.Shape();
     segmentGround3.graphics.beginFill(COLOR_GROUND)
@@ -247,7 +243,10 @@ function init() {
     var catEyes = new createjs.Shape();
     catEyes.graphics.beginFill('black')
         .drawCircle(-10, 14, 2)
-        .drawCircle(2, 14, 2);
+        .drawCircle(2, 14, 2)
+        .beginFill('pink')
+        .drawEllipse(-13, 17, 5, 2)
+        .drawEllipse(-1, 17, 5, 2);
     catWrapper.addChild(catEyes);
 
     var catEars = new createjs.Shape();
@@ -275,8 +274,8 @@ function init() {
     container3.addChild(beeWrapper);
 
     function container3Start() {
-        createjs.Tween.get(container2).wait(200).to({x: -1 * CIRCLE_RADIUS}, 500, createjs.Ease.circOut);
-        createjs.Tween.get(container3).wait(500).to({x: CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS}, 500, createjs.Ease.circOut)
+        createjs.Tween.get(container2).to({x: -1 * CIRCLE_RADIUS}, 500, createjs.Ease.circOut);
+        createjs.Tween.get(container3).to({x: CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS}, 500, createjs.Ease.circOut)
             .call(function () {
                 var beeDuration = 300;
                 createjs.Tween.get(bee)
@@ -297,43 +296,107 @@ function init() {
                     .to({y: 1}, beeDuration, createjs.Ease.sineOut)
                     .to({y: 0}, beeDuration, createjs.Ease.sineIn)
                     .to({y: -1}, beeDuration, createjs.Ease.sineOut)
-                    .to({y: 0}, beeDuration, createjs.Ease.sineIn);
+                    .to({y: 0}, beeDuration, createjs.Ease.sineIn)
+                    .wait(200)
+                    .call(container4Start);
             });
     }
 
-//    var circle2 = new createjs.Shape();
-//    circle2.graphics.beginFill('DeepSkyBlue').drawCircle(CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_RADIUS, CIRCLE_RADIUS);
-//    circles.push(circle2);
+    // Card 4: Turtle
+    var container4 = new createjs.Container();
+    container4.x = 3 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS;
+    container4.y = CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS;
 
-//    var circle3 = new createjs.Shape();
-//    circle3.graphics.beginFill('DeepSkyBlue').drawCircle(2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_RADIUS, CIRCLE_RADIUS);
-//    circles.push(circle3);
+    var segmentGround4 = new createjs.Shape();
+    segmentGround4.graphics.beginFill(COLOR_GROUND)
+        .arc(0, 0, CIRCLE_RADIUS, 30 * Math.PI / 180, 150 * Math.PI / 180);
+    container4.addChild(segmentGround4);
 
-//    var circle4 = new createjs.Shape();
-//    circle4.graphics.beginFill('DeepSkyBlue').drawCircle(CIRCLE_RADIUS, CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_RADIUS);
-//    circles.push(circle4);
+    var segmentSky4 = new createjs.Shape();
+    segmentSky4.graphics.beginFill(COLOR_SKY)
+        .arc(0, 0, CIRCLE_RADIUS, 150 * Math.PI / 180, 30 * Math.PI / 180);
+    container4.addChild(segmentSky4);
 
-//    var circle5 = new createjs.Shape();
-//    circle5.graphics.beginFill('DeepSkyBlue').drawCircle(CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_RADIUS);
-//    circles.push(circle5);
+    var turtleWrapper = new createjs.Container();
+    var turtle = new createjs.Shape();
+    turtle.graphics.beginFill('green').drawCircle(0, 15, 12);
+    turtleWrapper.addChild(turtle);
+    container4.addChild(turtleWrapper);
 
-//    var circle6 = new createjs.Shape();
-//    circle6.graphics.beginFill('DeepSkyBlue').drawCircle(2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_RADIUS);
-//    circles.push(circle6);
+    var container4a = new createjs.Container();
+    container4a.x = CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS;
+    container4a.y = -(2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS);
+    var sky = new createjs.Shape();
+    sky.graphics.beginFill('lightBlue')
+        .drawCircle(0, 0, CIRCLE_RADIUS);
+    container4a.addChild(sky);
 
-//    var circle7 = new createjs.Shape();
-//    circle7.graphics.beginFill('DeepSkyBlue').drawCircle(CIRCLE_RADIUS, 2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_RADIUS);
-//    circles.push(circle7);
+    var container4b = new createjs.Container();
+    container4b.x = CIRCLE_RADIUS;
+    container4b.y = -CIRCLE_RADIUS;
+//    container4b.y = CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS;
+    var sky4b = new createjs.Shape();
+    sky4b.graphics.beginFill('lightBlue')
+        .drawCircle(0, 0, CIRCLE_RADIUS);
+    container4b.addChild(sky4b);
+    var cloud4b = new createjs.Shape();
+    cloud4b.graphics
+        .beginFill('white')
+        .drawCircle(-20, 5, 13)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(-5, 12, 12)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(9, 12, 10)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(20, 4, 12)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(-7, -9, 14)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(13, -11, 10)
+        .endFill()
+        .beginFill('white')
+        // filler
+        .drawCircle(0, 5, 15)
+    ;
+    container4b.addChild(cloud4b);
 
-//    var circle8 = new createjs.Shape();
-//    circle8.graphics.beginFill('DeepSkyBlue').drawCircle(CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, 2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_RADIUS);
-//    circles.push(circle8);
+    var container4c = new createjs.Container();
+    container4c.x = 2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS;
+    container4c.y = -(CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS);
+    var sky4c = sky4b.clone();
+    container4c.addChild(sky4c);
+    var cloud4c = cloud4b.clone();
+    container4c.addChild(cloud4c);
 
-//    var circle9 = new createjs.Shape();
-//    circle9.graphics.beginFill('DeepSkyBlue').drawCircle(2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, 2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_RADIUS);
-//    circles.push(circle9);
+    function container4Start() {
+        createjs.Tween.get(container3).to({x: -1 * CIRCLE_RADIUS}, 500, createjs.Ease.circOut);
+        createjs.Tween.get(container4).to({x: CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS}, 500, createjs.Ease.circOut)
+            .wait(500).call(function () {
+                createjs.Tween.get(container4).to({y: 3 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS}, 1000, createjs.Ease.none);
+                createjs.Tween.get(turtleWrapper)
+                    .to({y: -(2 * (CIRCLE_DIAMETER + SPACING))}, 1000, createjs.Ease.none);
+                createjs.Tween.get(container4a)
+                    .to({y: CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS}, 400, createjs.Ease.linear)
+                    .call(function () {
 
-    circles.forEach(function (circle) {
-        stage.addChild(circle);
-    });
+                    });
+                createjs.Tween.get(container4b)
+                    .to({y: 4 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS}, 2000, createjs.Ease.linear);
+                createjs.Tween.get(container4c)
+                    .to({y: 3 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS}, 2000, createjs.Ease.linear);
+            });
+    }
+
+    stage.addChild(container1);
+    stage.addChild(container2);
+    stage.addChild(container3);
+    stage.addChild(container4);
+    stage.addChildAt(container4a, stage.getNumChildren() - 1);
+    stage.addChild(container4b);
+    stage.addChild(container4c);
 }
