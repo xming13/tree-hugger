80a0923c47e3b0a5c6337dd406fd9199e8638e7b
xming13 <xiaoming33@gmail.com>
2015-10-11 19:35:34 +0800

    convert to timeline 2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -146,7 +146,10 @@ function loadAudio() {
 //                });
             });
             pop.on('ended', function () {
-                setTimeout(renderMenu, 200);
+                setTimeout(function () {
+                    stage.removeChild(animationWrapper);
+                    renderMenu()
+                }, 200);
             });
         } else {
             console.log('request reached server but error is returned.');
@@ -256,24 +259,6 @@ function renderAnim() {
         pop.play();
     }
 
-    // Clipping Mask
-    var clipped_disk = new createjs.Shape();
-    clipped_disk.graphics.beginFill('#000000')
-//        .drawCircle(CIRCLE_RADIUS, CIRCLE_RADIUS, CIRCLE_RADIUS)
-//        .drawCircle(CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_RADIUS, CIRCLE_RADIUS)
-//        .drawCircle(2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_RADIUS, CIRCLE_RADIUS)
-//        .endFill()
-//        .beginFill('#00ff00')
-//        .drawCircle(CIRCLE_RADIUS, CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_RADIUS)
-        .drawCircle(CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_RADIUS)
-//        .drawCircle(2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, CIRCLE_RADIUS)
-//        .endFill()
-//        .beginFill('#00ff00')
-//        .drawCircle(CIRCLE_RADIUS, 2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_RADIUS)
-//        .drawCircle(CIRCLE_DIAMETER + SPACING + CIRCLE_RADIUS, 2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_RADIUS)
-//        .drawCircle(2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, 2 * (CIRCLE_DIAMETER + SPACING) + CIRCLE_RADIUS, CIRCLE_RADIUS)
-    ;
-
     var flowerObjs = getFlowerTimeline(LEFT_2, TOP_2);
     var flowerTimeline = flowerObjs[0];
     var container1 = flowerObjs[1];
@@ -308,129 +293,6 @@ function renderAnim() {
     container2.addChildAt(summerContainer, 0);
     container2.addChildAt(autumnContainer, 0);
     container2.addChildAt(winterContainer, 0);
-
-    // Card 2: Tree
-//    var container2 = new createjs.Container();
-//    container2.x = LEFT_4;
-//    container2.y = TOP_2;
-//
-//    var segmentGround2 = new createjs.Shape();
-//    segmentGround2.graphics.beginFill(COLOR_GROUND)
-//        .arc(0, 0, CIRCLE_RADIUS, 30 * Math.PI / 180, 150 * Math.PI / 180);
-//    container2.addChild(segmentGround2);
-//
-//    var segmentSky2 = new createjs.Shape();
-//    segmentSky2.graphics.beginFill(COLOR_SKY)
-//        .arc(0, 0, CIRCLE_RADIUS, 150 * Math.PI / 180, 30 * Math.PI / 180);
-//    container2.addChild(segmentSky2);
-
-//    DEVEL
-//    container2.x = LEFT_2;
-//    container2Start();
-
-//    function container2Start() {
-//        console.log('container2Start');
-//        createjs.Tween.get(container1).to({x: LEFT_0}, 500, createjs.Ease.circOut);
-//        createjs.Tween.get(container2).to({x: LEFT_2}, 500, createjs.Ease.circOut);
-//
-//        var subContainers = [];
-//        var groundFills = ['#59AE6B', '#FFFFB7', '#EB6907', '#E1F0EB'];
-//        var skyFills = ['#AEE7FB', '#C6FFEA', '#FAA105', '#322F40'];
-//
-//        for (var i = 0; i < 4; i++) {
-//            var _container = new createjs.Container();
-//            var _segmentGround = new createjs.Shape();
-//            _segmentGround.graphics.beginFill(groundFills[i])
-//                .arc(0, 0, CIRCLE_RADIUS, 30 * Math.PI / 180, 150 * Math.PI / 180);
-//            _container.addChild(_segmentGround);
-//            var _segmentSky = new createjs.Shape();
-//            _segmentSky.graphics.beginFill(skyFills[i])
-//                .arc(0, 0, CIRCLE_RADIUS, 150 * Math.PI / 180, 30 * Math.PI / 180);
-//            _container.addChild(_segmentSky);
-//
-//            subContainers.push(_container);
-//            container2.addChildAt(_container, 0);
-//        }
-//
-//        var dist = 1.5 * CIRCLE_RADIUS;
-//
-//        // DEVEL
-////        subContainers[0].x = -dist;
-////        subContainers[0].y = -dist;
-////        subContainers[1].x = dist;
-////        subContainers[1].y = -dist;
-////        subContainers[2].x = -dist;
-////        subContainers[2].y = dist;
-////        subContainers[3].x = dist;
-////        subContainers[3].y = dist;
-//
-//        // winter
-//        var snowTreeWrapper = new createjs.Container();
-//        var snowTree = new createjs.Shape();
-//        snowTree.graphics
-//            .beginFill('white')
-//            .lineTo(-12, -5)
-//            .lineTo(0, -20)
-//            .lineTo(12, -5)
-//            .endFill().beginFill('white')
-//            .lineTo(-14, 5)
-//            .lineTo(0, -10)
-//            .lineTo(14, 5)
-//            .endFill().beginFill('white')
-//            .lineTo(-16, 15)
-//            .lineTo(0, 0)
-//            .lineTo(16, 15)
-//            .endFill().beginFill('white')
-//            .lineTo(-18, 25)
-//            .lineTo(0, 10)
-//            .lineTo(18, 25);
-//        snowTreeWrapper.addChild(snowTree);
-//
-//        var snows = [];
-//        var snowWrapper = new createjs.Container();
-//        var snowSize = 3;
-//
-//        for (var i = 0; i < 9; i++) {
-//            var snow = new createjs.Shape();
-//            snow.alpha = 0;
-//            snow.graphics
-//                .beginFill('white')
-//                .drawCircle(0, 0, snowSize);
-//            snows.push(snow);
-//            snowWrapper.addChild(snow);
-//        }
-//
-//        function tweenSnow(snow) {
-//            var _random = Math.random();
-//            var _x = 0;
-//            if (_random <= 0.45) {
-//                _x = getRandomInt(-25, -10);
-//            }
-//            else if (_random <= 0.9) {
-//                _x = getRandomInt(10, 25);
-//            }
-//            else {
-//                _x = getRandomInt(-9, 9);
-//            }
-//
-//            createjs.Tween
-//                .get(snow, {override: true})
-//                .to({alpha: 0}, 100)
-//                .wait(getRandomInt(0, 2000))
-//                .to({alpha: getRandomArbitrary(0.5, 0.9), x: _x, y: getRandomInt(-55, -50)}, 0)
-//                .to({y: 24}, getRandomInt(2500, 3000), createjs.Ease.linear)
-//                .call(function () {
-//                    tweenSnow(snow);
-//                });
-//        }
-//
-//        snows.forEach(function (snow) {
-//            tweenSnow(snow);
-//        });
-//
-//        snowTreeWrapper.addChild(snowWrapper);
-//        subContainers[3].addChild(snowTreeWrapper);
-//
 
     var dist = 1.5 * CIRCLE_RADIUS;
     var subContainerDuration = .7;
@@ -505,6 +367,7 @@ function renderAnim() {
     var turtleObjs = getTurtleTimeline(LEFT_4, TOP_2);
     var turtleTimeline = turtleObjs[0];
     var container4 = turtleObjs[1];
+    var turtleWrapper = turtleObjs[2];
 
     var transitionTimeline3 = new TimelineMax();
     transitionTimeline3
@@ -512,202 +375,35 @@ function renderAnim() {
         .to(container3, .5, {x: LEFT_0, ease: Circ.easeOut}, 'transition3')
         .to(container4, .5, {x: LEFT_2, ease: Circ.easeOut}, 'transition3');
 
-//    createjs.Tween.get(subContainers[0]).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
-////                createjs.Tween.get(subContainers[1]).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
-////                createjs.Tween.get(subContainers[2]).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
-////                createjs.Tween.get(subContainers[3]).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut)
+    var container4a = new createjs.Container();
+    container4a.x = LEFT_2;
+    container4a.y = TOP_NEG1;
+    var sky = new createjs.Shape();
+    sky.graphics.beginFill('lightBlue')
+        .drawCircle(0, 0, CIRCLE_RADIUS);
+    container4a.addChild(sky);
 
-//        createjs.Tween.get(subContainers[0]).wait(subContainerDuration + subContainerInitialWait)
-//            .to({x: -dist, y: -dist}, subContainerDuration, createjs.Ease.cubicIn);
-//        createjs.Tween.get(subContainers[1]).wait(1.5 * subContainerDuration + subContainerInitialWait)
-//            .to({x: dist, y: -dist}, subContainerDuration, createjs.Ease.cubicIn);
-//        createjs.Tween.get(subContainers[2]).wait(2 * subContainerDuration + subContainerInitialWait)
-//            .to({x: -dist, y: dist}, subContainerDuration, createjs.Ease.cubicIn);
-//        createjs.Tween.get(subContainers[3]).wait(2.5 * subContainerDuration + subContainerInitialWait)
-//            .to({x: dist, y: dist}, subContainerDuration, createjs.Ease.cubicIn)
-//            .wait(1000)
-//            .call(function () {
-//                var duration = 300;
-//                createjs.Tween.get(subContainers[0]).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
-//                createjs.Tween.get(subContainers[1]).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
-//                createjs.Tween.get(subContainers[2]).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut);
-//                createjs.Tween.get(subContainers[3]).to({x: 0, y: 0}, duration, createjs.Ease.cubicOut)
-//                    .wait(200)
-//                    .call(function () {
-//                        snows.forEach(function (snow) {
-//                            createjs.Tween.removeTweens(snow);
-//                        });
-//                        container3Start();
-//                    });
-//            });
-//    }
+    var cloudObjs1 = getCloudTimeline(LEFT_1, TOP_0);
+    var container4b = cloudObjs1[1];
+    var cloudObjs2 = getCloudTimeline(LEFT_3, TOP_NEG1);
+    var container4c = cloudObjs2[1];
 
-    // Card 3: Cat
-//    var container3 = new createjs.Container();
-//    container3.x = LEFT_4;
-//    container3.y = TOP_2;
-//
-//    var segmentGround3 = segmentGround2.clone();
-//    container3.addChild(segmentGround3);
-//
-//    var segmentSky3 = segmentSky2.clone();
-//    container3.addChild(segmentSky3);
-//
-//    var catWrapper = new createjs.Container();
-//    var catHead = new createjs.Shape();
-//    catHead.graphics.beginFill('#fff').drawEllipse(-15, 5, 30, 25);
-//    catWrapper.addChild(catHead);
-//
-//    var catEyes = new createjs.Shape();
-//    catEyes.graphics.beginFill('black')
-//        .drawCircle(-10, 14, 2)
-//        .drawCircle(2, 14, 2)
-//        .beginFill('pink')
-//        .drawEllipse(-13, 17, 5, 2)
-//        .drawEllipse(-1, 17, 5, 2);
-//    catWrapper.addChild(catEyes);
-//
-//    var catEars = new createjs.Shape();
-//    catEars.graphics
-//        .beginFill('#fff')
-//        .moveTo(-15, 14)
-//        .lineTo(-10, 0)
-//        .lineTo(-1, 7)
-//        .closePath()
-//        .moveTo(15, 14)
-//        .lineTo(10, 0)
-//        .lineTo(1, 7)
-//        .closePath();
-//    catWrapper.addChild(catEars);
-//
-//    var beeWrapper = new createjs.Container();
-//    var bee = new createjs.Shape();
-//    bee.graphics
-//        .beginFill('yellow')
-//        .drawEllipse(-45, -20, 16, 12);
-//
-//    beeWrapper.addChild(bee);
-//
-//    container3.addChild(catWrapper);
-//    container3.addChild(beeWrapper);
-//
-//    function container3Start() {
-//        createjs.Tween.get(container2).to({x: LEFT_0}, 500, createjs.Ease.circOut);
-//        createjs.Tween.get(container3).to({x: LEFT_2}, 500, createjs.Ease.circOut)
-//            .call(function () {
-//                var beeDuration = 300;
-//                createjs.Tween.get(bee)
-//                    .to({x: 70}, beeDuration * 6, createjs.Ease.none);
-//                createjs.Tween.get(bee)
-//                    .to({y: -5}, beeDuration, createjs.Ease.sineOut)
-//                    .to({y: 0}, beeDuration, createjs.Ease.sineIn)
-//                    .to({y: 5}, beeDuration, createjs.Ease.sineOut)
-//                    .to({y: 0}, beeDuration, createjs.Ease.sineIn)
-//                    .to({y: -5}, beeDuration, createjs.Ease.sineOut)
-//                    .to({y: 0}, beeDuration, createjs.Ease.sineIn);
-//
-//                createjs.Tween.get(catEyes)
-//                    .to({x: 7}, beeDuration * 6, createjs.Ease.none);
-//                createjs.Tween.get(catEyes)
-//                    .to({y: -1}, beeDuration, createjs.Ease.sineOut)
-//                    .to({y: 0}, beeDuration, createjs.Ease.sineIn)
-//                    .to({y: 1}, beeDuration, createjs.Ease.sineOut)
-//                    .to({y: 0}, beeDuration, createjs.Ease.sineIn)
-//                    .to({y: -1}, beeDuration, createjs.Ease.sineOut)
-//                    .to({y: 0}, beeDuration, createjs.Ease.sineIn)
-//                    .wait(200)
-//                    .call(container4Start);
-//            });
-//    }
-
-//    // Card 4: Turtle
-//    var container4 = new createjs.Container();
-//    container4.x = LEFT_4;
-//    container4.y = TOP_2;
-//
-//    var segmentGround4 = segmentGround3.clone();
-//    container4.addChild(segmentGround4);
-//
-//    var segmentSky4 = segmentSky3.clone();
-//    container4.addChild(segmentSky4);
-//
-//    var turtleWrapper = new createjs.Container();
-//    var turtle = new createjs.Shape();
-//    turtle.graphics.beginFill('green').drawCircle(0, 15, 12);
-//    turtleWrapper.addChild(turtle);
-//    container4.addChild(turtleWrapper);
-//
-//    var container4a = new createjs.Container();
-//    container4a.x = LEFT_2;
-//    container4a.y = TOP_NEG1;
-//    var sky = new createjs.Shape();
-//    sky.graphics.beginFill('lightBlue')
-//        .drawCircle(0, 0, CIRCLE_RADIUS);
-//    container4a.addChild(sky);
-//
-//    var container4b = new createjs.Container();
-//    container4b.x = LEFT_1;
-//    container4b.y = TOP_0;
-//    var sky4b = sky.clone();
-//    container4b.addChild(sky4b);
-//    var cloud4b = new createjs.Shape();
-//    cloud4b.graphics
-//        .beginFill('white')
-//        .drawCircle(-20, 5, 13)
-//        .endFill()
-//        .beginFill('white')
-//        .drawCircle(-5, 12, 12)
-//        .endFill()
-//        .beginFill('white')
-//        .drawCircle(9, 12, 10)
-//        .endFill()
-//        .beginFill('white')
-//        .drawCircle(20, 4, 12)
-//        .endFill()
-//        .beginFill('white')
-//        .drawCircle(-7, -9, 14)
-//        .endFill()
-//        .beginFill('white')
-//        .drawCircle(13, -11, 10)
-//        .endFill()
-//        .beginFill('white')
-//        // filler
-//        .drawCircle(0, 5, 15)
-//    ;
-//    container4b.addChild(cloud4b);
-//
-//    var container4c = new createjs.Container();
-//    container4c.x = LEFT_3;
-//    container4c.y = TOP_NEG1;
-//    var sky4c = sky.clone();
-//    container4c.addChild(sky4c);
-//    var cloud4c = cloud4b.clone();
-//    container4c.addChild(cloud4c);
-//
-//    function container4Start() {
-//        createjs.Tween.get(container3).to({x: LEFT_0}, 500, createjs.Ease.circOut);
-//        createjs.Tween.get(container4).to({x: LEFT_2}, 500, createjs.Ease.circOut)
-//            .wait(500).call(function () {
-//                createjs.Tween.get(container4)
-//                    .to({y: TOP_4}, 1000, createjs.Ease.none);
-//                createjs.Tween.get(turtleWrapper)
-//                    .to({y: -(TOP_4 - TOP_2)}, 1000, createjs.Ease.none);
-//                createjs.Tween.get(container4a)
-//                    .to({y: TOP_2}, 400, createjs.Ease.linear);
-//                createjs.Tween.get(container4b)
-//                    .to({y: TOP_5}, 2000, createjs.Ease.linear);
-//                createjs.Tween.get(container4c)
-//                    .to({y: TOP_4}, 2000, createjs.Ease.linear);
-//            });
-//    }
+    var transitionTimeline4 = new TimelineMax();
+    transitionTimeline4
+        .add('transition4')
+        .to(container4, 1, {y: TOP_4, ease: Power0.easeNone}, 'transition4')
+        .to(turtleWrapper, 1, {y: -(TOP_4 - TOP_2), ease: Power0.easeNone}, 'transition4')
+        .to(container4a, .4, {y: TOP_2, ease: Power0.easeNone}, 'transition4')
+        .to(container4b, 2, {y: TOP_5, ease: Power0.easeNone}, 'transition4')
+        .to(container4c, 2, {y: TOP_4, ease: Power0.easeNone}, 'transition4')
 
     animationWrapper.addChild(container1);
     animationWrapper.addChild(container2);
     animationWrapper.addChild(container3);
     animationWrapper.addChild(container4);
-//    animationWrapper.addChildAt(container4a, animationWrapper.getNumChildren() - 1);
-//    animationWrapper.addChild(container4b);
-//    animationWrapper.addChild(container4c);
+    animationWrapper.addChildAt(container4a, animationWrapper.getNumChildren() - 1);
+    animationWrapper.addChild(container4b);
+    animationWrapper.addChild(container4c);
 
     mainTimeline = new TimelineMax({paused: true});
     mainTimeline
@@ -719,6 +415,8 @@ function renderAnim() {
         .add(transitionTimeline2, '+=0.2')
         .add(catTimeline, '+=0')
         .add(transitionTimeline3, '+=0.2')
+        .add(turtleTimeline, '+=0')
+        .add(transitionTimeline4, '+=0.5')
         .play();
 }
 
@@ -817,6 +515,16 @@ function _renderGallery() {
                 this.restart();
             });
             turtleTimeline.play();
+
+            var cloudObjs = getCloudTimeline(LEFT_3, TOP_1);
+            var cloudTimeline = cloudObjs[0];
+            var cloudContainer = cloudObjs[1];
+            galleryWrapper.addChild(cloudContainer);
+            cloudTimeline.eventCallback('onComplete', function () {
+                this.restart();
+            });
+            cloudTimeline.play();
+
             break;
         case 3:
             break;
@@ -1360,7 +1068,48 @@ function getTurtleTimeline(x, y) {
     turtleWrapper.addChild(turtle);
     turtleContainer.addChild(turtleWrapper);
 
-    return [turtleTimeline, turtleContainer];
+    return [turtleTimeline, turtleContainer, turtleWrapper];
+}
+
+function getCloudTimeline(x, y) {
+    var cloudTimeline = new TimelineMax();
+
+    var cloudContainer = new createjs.Container();
+    cloudContainer.x = x;
+    cloudContainer.y = y;
+
+    var sky = new createjs.Shape();
+    sky.graphics.beginFill('lightBlue')
+        .drawCircle(0, 0, CIRCLE_RADIUS);
+    cloudContainer.addChild(sky);
+
+    var cloud = new createjs.Shape();
+    cloud.graphics
+        .beginFill('white')
+        .drawCircle(-20, 5, 13)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(-5, 12, 12)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(9, 12, 10)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(20, 4, 12)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(-7, -9, 14)
+        .endFill()
+        .beginFill('white')
+        .drawCircle(13, -11, 10)
+        .endFill()
+        .beginFill('white')
+        // filler
+        .drawCircle(0, 5, 15)
+    ;
+    cloudContainer.addChild(cloud);
+
+    return [cloudTimeline, cloudContainer];
 }
 
 // Utilities
